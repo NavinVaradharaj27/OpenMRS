c027e39bc211cea8f7c9dc6a8ced5b15550df8d2
unknown <ksandhyareddy95@gmail.com>
2024-10-20 13:21:21 +0530

    created method

--- src/main/java/com/OpenMRS/base/Baseclass.java
+++ src/main/java/com/OpenMRS/base/Baseclass.java
@@ -1,5 +1,19 @@
 package com.OpenMRS.base;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Baseclass {
+	public static WebDriver driver;
+	protected static void pageNavigate(String Url)
+	{
+	try {
+		driver.navigate().to(Url);
+	} 
+	catch (Exception e) {
+		e.printStackTrace();
+	}
+	}
+
 
 }
